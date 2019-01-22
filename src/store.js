674b29d02c3ff1f2aa6d68769569d0d11631c351
Cam Prier <CSPrier@gmail.com>
2019-01-22 18:07:28 -0500

    Edited public/index.html and cleaned up commented lines

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import characterReducer from './reducers/characterReducer';
 
+const rootReducer = combineReducers({
+  // form: formReducer,
+  // auth: authReducer,
+  characters: characterReducer
+});
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(characterReducer, composeEnhancers(
+const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
   ));
 
