ac259d1a414ccfe36be6bed2b3860f4010b833e6
Cam Prier <CSPrier@gmail.com>
2019-01-23 20:26:34 -0500

    process.env.PUBLIC_KEY is still undefined...

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export const PRIVATE_KEY = process.env.PRIVATE_KEY;
-export const PUBLIC_KEY = process.env.PUBLIC_KEY;
-export const MARVEL_URL = 'https://gateway.marvel.com/v1/public'
+// require('dotenv').config()
+export const PRIVATE_KEY=process.env.PRIVATE_KEY;
+export const PUBLIC_KEY=process.env.PUBLIC_KEY;
+export const MARVEL_URL='https://gateway.marvel.com/v1/public'
+// console.log(`config: ${JSON.stringify(process.env, null, 2)}`);
