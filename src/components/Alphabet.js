9a81f2e46d5e993bd5f814491aabfe5d2202ace1
Cam Prier <CSPrier@gmail.com>
2019-01-25 17:27:30 -0500

    Quick refactor of buttons variable on line 11

--- src/components/Alphabet.js
+++ src/components/Alphabet.js
@@ -6,12 +6,14 @@ class Alphabet extends Component {
   state = {
     alphabet: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   }
+  
   render() {
     const buttons = this.state.alphabet.map(letter => {
-      return <li className="alphabet-item">
-               <button>{letter}</button>
-             </li>
-            })
+      return (
+        <li className="alphabet-item">
+          <button>{letter}</button>
+        </li>
+      )});
     return(
       <div className="alphabet-selector-container">
         <div className="alphabet-selector">
