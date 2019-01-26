85918f87c2a254f424bdfe27b155d90c5b0b2625
Cam Prier <CSPrier@gmail.com>
2019-01-26 16:14:57 -0500

    Added Link to landing page but need to css position it to overlay on top of landingpage image and such

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './css/landingpage.css';
+import { Link } from 'react-router-dom';
 
 class LandingPage extends Component {
   render() {
     return(
       <div className="langing-page-container">
         <div className="landing-page">
+          {/* <img src={require('./css/images/marvelcollage.jpg')} alt="landingpage"></img> */}
+          <div className="move-to-dashboard">
+            <Link to='/Dashboard'>To the Dashboard</Link>
+          </div>
           <img src={require('./css/images/marvelcollage.jpg')} alt="landingpage"></img>
         </div>
       </div>
