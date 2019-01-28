df464330e39e6ca0148d5321694de19b3bf800bd
Cam Prier <CSPrier@gmail.com>
2019-01-28 12:14:24 -0500

    React-router is working to get from landing page to dashboard components. Merging to master branch

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import './css/landingpage.css';
-import { Link } from 'react-router-dom';
 
 class LandingPage extends Component {
+  goToBoard(e) {
+    e.preventDefault();
+    this.props.history.push('/dashboard');
+  }
+
   render() {
     return(
-      <div className="langing-page-container">
-        <div className="landing-page">
-          {/* <img src={require('./css/images/marvelcollage.jpg')} alt="landingpage"></img> */}
-          <div className="move-to-dashboard">
-            <Link to='/Dashboard'>To the Dashboard</Link>
-          </div>
-          <img src={require('./css/images/marvelcollage.jpg')} alt="landingpage"></img>
-        </div>
+      <div className="landing-page">
+        {/* <img src={require('./css/images/marvelcollage.jpg')} alt="landingpage"></img> */}
+        <form onSubmit={e => this.goToBoard(e)} className="landing-page-form">
+          <button className="marvel-button">Go to the Dashboard</button>
+        </form>
       </div>
     );
   }
