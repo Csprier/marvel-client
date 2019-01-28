911ce15a7b27a5c051f3eb407ad8621261fcce0e
Cam Prier <CSPrier@gmail.com>
2019-01-28 17:35:48 -0500

    Made a returnToLandingPage button on the Navigation on the Dashboard

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+// import { Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './css/navigation.css';
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.goToLandingPage = this.goToLandingPage.bind(this);
+  }
+
+  goToLandingPage({history}) {
+    this.props.history.goBack();
+  }
+
   render() {
     return(
       <div className="navigation-container">
         <h1>Marvel API</h1>
+        <button onClick={this.props.history.goBack} className="marvel-button">Return</button>
       </div>
     );
   }
 }
 
-export default Navigation;
+export default withRouter(Navigation);
