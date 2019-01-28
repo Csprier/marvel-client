de276d6beb480ef5e4ab296d2b63b0e6ac02ff5b
Cam Prier <CSPrier@gmail.com>
2019-01-28 12:56:17 -0500

    added the starting styles for mobile design on landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -10,9 +10,14 @@ class LandingPage extends Component {
   render() {
     return(
       <div className="landing-page">
-        <form onSubmit={e => this.goToBoard(e)} className="landing-page-form">
-          <button className="marvel-button">Go to the Dashboard</button>
-        </form>
+        <div className="landing-page-info-container">
+          <div className="landing-page-info">
+            <span>Powered by <a href="https://www.marvel.com/">Marvel's</a> Portal API, this app aims to be a one-stop-shop for Super Hero information! Search your favorite heros and see what they are about, what comics there are and which they have appeared in! Who their creators were, and more!</span>
+          </div>
+          <form onSubmit={e => this.goToBoard(e)} className="landing-page-form">
+            <button className="marvel-button">Go to the Dashboard</button>
+          </form>
+        </div>
       </div>
     );
   }
