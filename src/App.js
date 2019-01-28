8fc7376170b7319444c2d81fce36c22cabb7cd77
Cam Prier <CSPrier@gmail.com>
2019-01-28 17:06:57 -0500

    Modified some styles for the landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/Navigation';
+// import Navigation from './components/Navigation';
 import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
 
@@ -10,11 +10,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <h1><Link to="/">Marvel API</Link></h1>
-          </header>
           <main>
-              <Navigation />
               <Route exact path="/" component={LandingPage} />
               <Route exact path="/dashboard" component={Dashboard} />
           </main>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Characters from './Characters';
+import Navigation from './Navigation';
+import './css/dashboard.css';
 
 class Dashboard extends Component {
   render() {
     return(
       <div className="dashboard-container">
+        <Navigation />
+        <h1>Dashboard</h1>
         <Characters />
       </div>
     );
