3e9bb3e0bba96569052a783ea589fee93fd413e8
Cam Prier <CSPrier@gmail.com>
2019-02-05 18:05:05 -0500

    Trying to correct the styles of the character list item elements in Characters.js and organized the character.css file

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -21,26 +21,32 @@ class Characters extends Component {
   }
 
   render() {
-    const characterDummyInfo = this.props.characters.map((character, i) => {
+    const characterInfo = this.props.characters.map((character, i) => {
       return (
         <li key={i} className="character-list-item">
           <div className="character-item">
             <div className="character-header">
               <h3>{character.name}</h3>
             </div>
-            {(character.thumbnail) 
-                ? <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} className="character-thumbnail" alt="" /> 
-                : <img src={'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'} className="character-thumbnail" alt="no thumbnail available" />}
-            <div className="character-description">
-              <p>ID: {character.id}</p>  
-              <h4>Description:</h4>
-              <p>{character.description.length !== 0 ? character.description : '~Description not available~'}</p>  
-              <div className="character-urls">
-                <h4>URLs to various external links:</h4>
-                <ul>{character.urls.map((url, i) => <li key={i}><a href={url.url}>{url.type}</a></li>)}</ul>  
+
+            <div className="character-item-info-container">
+              {(character.thumbnail) 
+                  ? <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} className="character-thumbnail" alt="" /> 
+                  : <img src={'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'} className="character-thumbnail" alt="no thumbnail available" />}
+              
+              <div className="character-description">
+                <p>ID: {character.id}</p>  
+                <h4>Description:</h4>
+                <p>{character.description.length !== 0 ? character.description : '~Description not available~'}</p>  
+                <div className="character-urls">
+                  <h4>URLs to various external links:</h4>
+                  <ul>{character.urls.map((url, i) => <li key={i}><a href={url.url}>{url.type}</a></li>)}</ul>  
+                </div>
+                <p className="attributionText">{character.attributionText}</p>
               </div>
-              <p className="attributionText">{character.attributionText}</p>
+
             </div>
+
           </div>
         </li>
       )});
@@ -54,7 +60,7 @@ class Characters extends Component {
         </div>
         <div className="character-info">
           <ul>
-            {characterDummyInfo}
+            {characterInfo}
           </ul>
         </div>
       </div>
