c610420f328ecf2524d01ebe66e41496bfccf39e
Cam Prier <CSPrier@gmail.com>
2019-02-13 14:41:45 -0500

    Added a return to dashboard button to characters and styled the component to accomdate it

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,8 +16,10 @@ class Navigation extends Component {
     return(
       <div className="navigation-container">
         <h1>Marvel API</h1>
-        <button className="logout-button" onClick={this.onClickLogout}>LogOut</button>
-        <button onClick={this.props.history.goBack} className="return-button">Return</button>
+        <button 
+          className="logout-button" 
+          onClick={this.onClickLogout}
+        >LogOut</button>
       </div>
     );
   }
