76c64380bc5938b4c690a48bf9fa9bd7f3aa1319
Cam Prier <CSPrier@gmail.com>
2019-02-20 13:58:51 -0500

    reorganized local-storage.js functions and added comments

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,13 +1,14 @@
-export const loadAuthToken = () => {
-  return localStorage.getItem('authToken');
-};
-
 export const saveAuthToken = authToken => {
   try {
     // .setItem method saves key/value pairs in the local storage in the browser, both as strings.
     // .setItem(key, value);
     localStorage.setItem('authToken', authToken);
   } catch(e) {}
+};
+
+// .getItem(value) gets the value from the localStorage, if it exists, otherwise, it returns null.
+export const loadAuthToken = () => {
+  return localStorage.getItem('authToken');
 };
 
 export const clearAuthToken = () => {
